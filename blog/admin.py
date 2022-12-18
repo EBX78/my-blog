615c4571d090d32aec663e693e2ad937255642f3
Abdullah Amiri <abdullah.7901741@gmail.com>
2022-12-18 20:45:27 +0330

    add category to article fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('position', 'title', 'slug', 'status')    # show article info and sort with this info
+    list_filter = (['status'])    # filter articles
+    search_fields = ('title', 'slug')    # add search field
+    prepopulated_fields = {'slug':('title',)}    # auto fill 'slug' field with 'title' content
+
+admin.site.register(Category, CategoryAdmin)    # register models in admin panel
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'jpublish', 'status')    # show article info and sort with this info
+    list_display = ('title', 'slug', 'jpublish', 'status', 'scategory')    # show article info and sort with this info
     list_filter = ('publish', 'status')    # filter articles
     search_fields = ('title', 'description')    # add search field
     prepopulated_fields = {'slug':('title',)}    # auto fill 'slug' field with 'title' content
     ordering = ['status', '-publish']    # set default sort for articles
+
+    def scategory(self, obj):
+        return [category.title for category in obj.category.all()]
+    scategory.short_description = "دسته بندی"
 
 admin.site.register(Article, ArticleAdmin)    # register models in admin panel
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,22 @@ from django.utils import timezone
 from extentions.utils import jalali_converter
 
 # recive data from admin panel with this template
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان دسته بندی')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس دسته بندی')
+    status = models.BooleanField(default=True, verbose_name='أیا نمایش داده شود؟')
+    position = models.IntegerField(verbose_name='پوزیشن')
+
+    # change Article title name to persian name
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+        ordering = ['position']
+
+    # show articles by title
+    def __str__(self):
+        return self.title
+
 class Article(models.Model):
     STATUS_CHOICES = (
         ('p', 'منتشر شده'),
@@ -10,6 +26,7 @@ class Article(models.Model):
     )
     title = models.CharField(max_length=200, verbose_name='عنوان')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس مقاله')
+    category = models.ManyToManyField(Category, verbose_name='دسته بندی')
     description = models.TextField(verbose_name='توضیحات')
     thumbnail = models.ImageField(upload_to='images', verbose_name='تصویر')
     publish = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
@@ -17,18 +34,15 @@ class Article(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name='وضعیت')
 
-    # change Article title name to persian name
     class Meta:
         verbose_name = 'مقاله'
         verbose_name_plural = 'مقالات'
 
-    # show articles by title 
     def __str__(self):
         return self.title
 
     # change publish datetime to local calender
     def jpublish(self):
     	return jalali_converter(self.publish)
-
-    # change jpublish column name in articl list
+    # change jpublish column name in article list
     jpublish.short_description = "زمان انتشار"
