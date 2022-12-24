82c9296cadca6e4ec8dc987dd176d42c69317636
Abdullah Amiri <abdullah.7901741@gmail.com>
2022-12-24 11:27:39 +0330

    add category to each article in home page

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from .models import *
+from .models import Article, Category
 
 # view of home page
 def home(request):
 	context = {
-		'articles': Article.objects.filter(status='p').order_by('-publish')[:]    # get articles from model, for display in template
+		'articles': Article.objects.filter(status='p'),    # get articles from model, for display in template
+		'categories': Category.objects.filter(status=True)
 	}
 	return render(request, 'blog/home.html', context)
 
 # view of each article page
 def detail(request, slug):
 	context = {
-		'article': get_object_or_404(Article, slug=slug, status='p')    # get the article that called as slug in url with 404 erorr handler
+		'article': get_object_or_404(Article, slug=slug, status='p'),    # get the article that called as slug in url with 404 erorr handler
+		'categories': Category.objects.filter(status=True)
 	}
 	return render(request, 'blog/detail.html', context)
