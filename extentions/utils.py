b8fc9c10621094dd2cd2c167503869d6d0932f0a
Abdullah Amiri <abdullah.7901741@gmail.com>
2022-12-24 15:44:27 +0330

    right align elements

--- extentions/utils.py
+++ extentions/utils.py
@@ -14,7 +14,7 @@ def jalali_converter(time):
 	time_to_str = "{},{},{}".format(time.year, time.month, time.day)
 	time_to_tuple = jalali.Gregorian(time_to_str).persian_tuple()
 
-	output = "{} {} {} ساعت {}: {}".format(
+	output = "{} {} {} ساعت {}:{}".format(
 		time_to_tuple[2],
 		month_to_str[f'{time_to_tuple[1]}'],
 		time_to_tuple[0],
